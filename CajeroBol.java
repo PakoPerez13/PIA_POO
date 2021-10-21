3bb4b5cc1f079c6d4027774056202035ed0fad0c
PakoPerez13 <89920305+PakoPerez13@users.noreply.github.com>
2021-10-20 23:56:56 -0500

    Add files via upload

---
+++ CajeroBol.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class CajeroBol extends Empleado {
+	Scanner Scan = new Scanner(System.in);
+	int Cantidad = 0, op = 0;
+	float valor = 0;
+	Gerente gerente;
+	Pelicula pelicula = new Pelicula();
+	
+	void IniciarPedido() {
+		
+		Fun.MostrarPeliculas();
+		
+		System.out.println("Introduzca los datos que se le piden para entregarle su boleto \n");
+		
+		System.out.println("Pelicula que seleccionara: ");
+		pelicula.setNombre(Scan.nextLine());
+		
+		System.out.println("Cuantos boletos desea comprar?: ");
+		Cantidad = Scan.nextInt();
+		
+		System.out.println("Introduzca el precio individual del boleto: ");
+		valor = Scan.nextFloat();
+		
+		pelicula.setPrecio(Cantidad*valor);
+		
+		System.out.println("Que desea hacer? \n");
+		System.out.println("1.- Reiniciar Pedido \n");
+		System.out.println("2.- Terminar Pedido \n");
+		op = Scan.nextInt();
+		
+		switch(op)
+		{
+		case 1:
+			ReiniciarPedido();
+			break;
+		case 2:
+			TerminarPedido();
+			break;
+		
+		}
+		
+	}
+	
+	void ReiniciarPedido() {
+		
+		
+		IniciarPedido();
+	
+	}
+
+	void MostrarPeliculas() {
+	
+		Fun.MostrarPeliculas();
+	}
+	
+	void TerminarPedido() {
+		System.out.println("Su pedido es el siguiente: ");
+		System.out.println("Funcion: " + pelicula.getNombre() + " cantidad de boletos: " + Cantidad + " Total a pagar: " + pelicula.getPrecio());
+		System.out.println("Gracias por su compra:) ");
+		valor = pelicula.getPrecio();
+		Fun.Dinero.setGanancias((Fun.Dinero.getGanancias()+valor));
+	}
+
+}
---
+++ Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Declaración de los Objetos que se usaran
+		Funcionalidades Fun = new Funcionalidades();
+		Menus M = new Menus();
+		Scanner Scan = new Scanner(System.in);
+		int Tipo = 0;
+		Fun.CrearArchivoEmpleados();
+		
+		do {
+		System.out.printf("Bienvenido al programa, por favor, seleccione el menu al que quiera acceder: \n");
+		System.out.println("1.- Gerente \n");
+		System.out.println("2.- Cajero Boletos \n");
+		System.out.println("3.- Cajero Dulceria \n");
+		System.out.println("4.- Para terminar el programa \n");
+		Tipo = Scan.nextInt();
+		
+		
+			switch(Tipo) {
+			case 0:
+				System.out.printf("Seleccione una opcion validad\n");
+				break;
+			case 1:
+				M.MenuGerente();
+				break;
+			case 2:
+				M.MenuCajeroBol();
+				break;
+			case 3:
+				M.MenuCajeroDul();
+				break;
+			case 4:
+				System.out.printf("Se terminará el programa, gracias por usarlo \n");
+				break;
+			}
+		}while(Tipo != 4);
+		
+		Scan.close();
+	}
+	
+
+}
