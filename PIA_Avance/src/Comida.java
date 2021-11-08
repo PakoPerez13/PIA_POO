99c86e3ca0b4b023ea2220d9b5836f573877e97e
PakoPerez13 <89920305+PakoPerez13@users.noreply.github.com>
2021-11-07 23:27:27 -0600

    Add files via upload

---
+++ PIA_Avance/src/Comida.java
@@ -0,0 +1,22 @@
+
+public class Comida extends Producto {
+	
+	String Tipo;
+	float Gasto;
+	
+	public String getTipo() {
+		return Tipo;
+	}
+	public void setTipo(String tipo) {
+		Tipo = tipo;
+	}
+	public float getGasto() {
+		return Gasto;
+	}
+	public void setGasto(float gasto) {
+		Gasto = gasto;
+	}
+	
+	
+
+}
---
+++ PIA_Avance/src/Pelicula.java
@@ -0,0 +1,37 @@
+
+public class Pelicula extends Producto {
+
+	String Tiempo;
+	int Sala;
+	String Asiento;
+	
+	void Gasto(float dinero){
+		
+	}
+
+	public String getTiempo() {
+		return Tiempo;
+	}
+
+	public void setTiempo(String tiempo) {
+		Tiempo = tiempo;
+	}
+
+	public int getSala() {
+		return Sala;
+	}
+
+	public void setSala(int sala) {
+		Sala = sala;
+	}
+
+	public String getAsiento() {
+		return Asiento;
+	}
+
+	public void setAsiento(String asiento) {
+		Asiento = asiento;
+	}
+
+	
+}
